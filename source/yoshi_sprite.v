5134f20b52166a2fcdb7618c1b5dcbec8fb7c5b0
Joey Conenna <jconenna@knights.ucf.edu>
2016-11-22 14:20:17 -0500

    Update yoshi_sprite.v

--- source/yoshi_sprite.v
+++ source/yoshi_sprite.v
@@ -4,21 +4,21 @@ module yoshi_sprite
         input wire btnU, btnL, btnR, // inputs used to move sprite across screen
         input wire video_on,         // input from vga_sync signaling when video signal is on
         input wire [9:0] x, y,       // current pixel coordinates from vga_sync circuit
-		input wire grounded,         // input signal conveying when Yoshi is grounded on a platform
-	    input wire game_over_yoshi,  // input signal conveying when game state is gameover
-		input wire collision,        // input signal conveying when yoshi collides with ghost
+	input wire grounded,         // input signal conveying when Yoshi is grounded on a platform
+	input wire game_over_yoshi,  // input signal conveying when game state is gameover
+	input wire collision,        // input signal conveying when yoshi collides with ghost
         output reg [11:0] rgb_out,   // output rgb signal for current yoshi pixel
         output reg yoshi_on,         // output signal asserted when input x/y are within yoshi sprite in display area
         output wire [9:0] y_x, y_y,  // output signals for yoshi sprite's current location within display area
-		output wire jumping_up,      // output signal asserted when yoshi is jumping up
-		output wire direction        // output signal conveying yoshi's direction of motion
+	output wire jumping_up,      // output signal asserted when yoshi is jumping up
+	output wire direction        // output signal conveying yoshi's direction of motion
     );
    
     // constant declarations
     // pixel coordinate boundaries for VGA display area
     localparam MAX_X = 640;
     localparam MAX_Y = 480;
-	localparam MIN_Y =  16;
+    localparam MIN_Y =  16;
    
     // tile width and height
     localparam T_W = 16;
@@ -86,19 +86,19 @@ module yoshi_sprite
    
     // symbolic state representations for FSM
     localparam [1:0] no_dir = 2'b00,
-                       left = 2'b01,
-                      right = 2'b10;      
+                     left = 2'b01,
+                    right = 2'b10;      
 
-	// to simulate x axis motion and momentum there is a countdown register x_time_reg that must decrement
+    // to simulate x axis motion and momentum there is a countdown register x_time_reg that must decrement
     // on clk edges to 0 between sprite position updates. The initial value sets the speed of motion. This
     // register decrements from a smaller value each successive move when a directional button is held, such that 
     // the sprite will slowly speed up to a maximum speed, which is given by a minimum countdown time value. 
-	// When yoshi is grounded, the momentum in x can change to another direction instantaneously. When he is 
-	// in the air and moving in a particular direction, momentum in that x direction is constant and he will
-	// continue to follow a parabolic trajectory through space. The x momentum can be adjusted  midair by pressing
-	// the button opposite of the current direction, but this change follows the 2D game physics standard that has
-	// the momentum slowly decrease until a minimum until it begins increasing in the new direction. This allows
-	// yoshi to change his motion midair but with a delay due to overcoming initial momentum. 	
+    // When yoshi is grounded, the momentum in x can change to another direction instantaneously. When he is 
+    // in the air and moving in a particular direction, momentum in that x direction is constant and he will
+    // continue to follow a parabolic trajectory through space. The x momentum can be adjusted  midair by pressing
+    // the button opposite of the current direction, but this change follows the 2D game physics standard that has
+    // the momentum slowly decrease until a minimum until it begins increasing in the new direction. This allows
+    // yoshi to change his motion midair but with a delay due to overcoming initial momentum. 	
                      
     // constant parameters that determine x direction speed              
     localparam TIME_START_X  =   800000;  // starting value for x_time & x_start registers
@@ -161,7 +161,7 @@ module yoshi_sprite
                     if(s_x_reg >= 17)                                           // is sprite can move left,
                         s_x_next = s_x_reg - 1;                                 // move left
                     
-					if(btnL && x_start_reg > TIME_MIN_X)                        // if left button pressed and x_start_reg > min,
+		    if(btnL && x_start_reg > TIME_MIN_X)                    	// if left button pressed and x_start_reg > min,
                         begin                                                   // make sprite move faster in x direction,
                         x_start_next = x_start_reg - TIME_STEP_X;               // set x_start_reg to decremented start time
                         x_time_next  = x_start_reg - TIME_STEP_X;               // set x_time_reg to decremented start time
@@ -183,49 +183,49 @@ module yoshi_sprite
                     x_state_next = no_dir;                                      // go to no direction state
                 else if(!grounded && btnR && x_start_reg >= TIME_START_X)       // if mid air and right button pressed and left momentum minimized
                     begin
-					x_state_next = right;                                       // go to right state and start moving right
-					x_time_next  = TIME_START_X;                                // set x_time reg to start time
+		    x_state_next = right;                                       // go to right state and start moving right
+		    x_time_next  = TIME_START_X;                                // set x_time reg to start time
                     x_start_next = TIME_START_X;                                // set start time reg to start time
-					end
-				end
+		    end
+		end
 			
-			right:
+	    right:
                 begin
-				if(x_time_reg > 0)                                              // if x_time reg > 0,
-					x_time_next = x_time_reg - 1;                               // decrement
+		if(x_time_reg > 0)                                              // if x_time reg > 0,
+			x_time_next = x_time_reg - 1;                           // decrement
 				
-				else if(x_time_reg == 0)                                        // if x_time reg = 0
-					begin
-					if(s_x_reg + 1 < MAX_X - T_W - X_D - 15)                    // is sprite can move right,
-						s_x_next = s_x_reg + 1;                                 // move right
+		else if(x_time_reg == 0)                                        // if x_time reg = 0
+			begin
+			if(s_x_reg + 1 < MAX_X - T_W - X_D - 15)                // is sprite can move right,
+			s_x_next = s_x_reg + 1;                                 // move right
 						
-					if(btnR && x_start_reg > TIME_MIN_X)                        // if right button pressed and x_start_reg > min,
-						begin                                                   // make sprite move faster in x direction,
-						x_start_next = x_start_reg - TIME_STEP_X;               // set x_start_reg to decremented start time
-						x_time_next  = x_start_reg - TIME_STEP_X;               // set x_time_reg to decremented start time
-						end
-					else if(btnL && x_start_reg < TIME_START_X)                 // if sprite isnt on ground, and left button is pressed,
-						begin                                                   // and x_start_reg is < start time, slow down left motion
-						x_start_next = x_start_reg + TIME_STEP_X;               // set x_start_reg to incremented start time
-						x_time_next  = x_start_reg + TIME_STEP_X;               // set x_time_reg  to incremented start time
-						end
+			if(btnR && x_start_reg > TIME_MIN_X)                    // if right button pressed and x_start_reg > min,
+				begin                                           // make sprite move faster in x direction,
+				x_start_next = x_start_reg - TIME_STEP_X;       // set x_start_reg to decremented start time
+				x_time_next  = x_start_reg - TIME_STEP_X;       // set x_time_reg to decremented start time
+				end
+			else if(btnL && x_start_reg < TIME_START_X)             // if sprite isnt on ground, and left button is pressed,
+				begin                                           // and x_start_reg is < start time, slow down left motion
+				x_start_next = x_start_reg + TIME_STEP_X;       // set x_start_reg to incremented start time
+				x_time_next  = x_start_reg + TIME_STEP_X;       // set x_time_reg  to incremented start time
+				end
 						
-					else                                                        // else right motion stays the same
-						begin
-						x_start_next = x_start_reg;                             // x_start_reg stays the same
-						x_time_next  = x_start_reg;                             // x_time_reg  stays the same
-						end
-					end
+			else                                                        // else right motion stays the same
+				begin
+				x_start_next = x_start_reg;                         // x_start_reg stays the same
+				x_time_next  = x_start_reg;                         // x_time_reg  stays the same
+				end
+			end
 				
-				if(grounded && (!btnR || (btnL && btnR)))                       // if yoshi grounded, and right button unpressed, or both pressed
-					x_state_next = no_dir;                                      // go to no direction state
-				else if(!grounded && btnL && x_start_reg >= TIME_START_X)       // if mid air and left button pressed and right momentum minimized
-					begin
-					x_state_next = left;                                        // go to left state and start moving left
-					x_time_next  = TIME_START_X;                                // set x_time reg to start time
-                    x_start_next = TIME_START_X;                                // set start time reg to start time
-					end
-				end	
+		if(grounded && (!btnR || (btnL && btnR)))                       // if yoshi grounded, and right button unpressed, or both pressed
+			x_state_next = no_dir;                                  // go to no direction state
+		else if(!grounded && btnL && x_start_reg >= TIME_START_X)       // if mid air and left button pressed and right momentum minimized
+			begin
+			x_state_next = left;                                        // go to left state and start moving left
+			x_time_next  = TIME_START_X;                                // set x_time reg to start time
+                        x_start_next = TIME_START_X;                                // set start time reg to start time
+			end
+		end	
 		endcase
 		end    
                    
@@ -233,18 +233,18 @@ module yoshi_sprite
     /*              FSMD for Sprite standing/walking states, and y motion              */  
     /***********************************************************************************/  
    
-	// motion in the y dimension follows a scheme that simulates gravity with a terminal velocity. When the jump/up
-	// button is pressed it loads a start countdown value to the jump_t_reg, wich decrements on clk edges until 0,
-	// when the y position is updated. Between each y position update the start time loaded to the register increases
-	// which slows down the sprite's y position updates until a max time value is reached, upon which the sprite will
-	// move downward. While moving downward, the jump_t_reg decrements to 0, when it updates the sprite position. Between
-	// position updates, the loaded time to jump_t_reg decreses until reaching a terminal value. In effect, this scheme 
-	// allows yoshi to jump up, slowing down until reaching a peak, then falling down, speeding up until reaching a terminal
-	// falling speed. 
-	// When jump/up button is initially pressed, the duration it is held increments the extra_up_reg, which will allow yoshi
-	// to jump higher depending on the time the button is held. 
-	// Since jumping requires yoshi to be drawn differently depending on whether going up or down, how yoshi is drawn standing 
-	// or while walking is determined in the same FSM.
+    // motion in the y dimension follows a scheme that simulates gravity with a terminal velocity. When the jump/up
+    // button is pressed it loads a start countdown value to the jump_t_reg, wich decrements on clk edges until 0,
+    // when the y position is updated. Between each y position update the start time loaded to the register increases
+    // which slows down the sprite's y position updates until a max time value is reached, upon which the sprite will
+    // move downward. While moving downward, the jump_t_reg decrements to 0, when it updates the sprite position. Between
+    // position updates, the loaded time to jump_t_reg decreses until reaching a terminal value. In effect, this scheme 
+    // allows yoshi to jump up, slowing down until reaching a peak, then falling down, speeding up until reaching a terminal
+    // falling speed. 
+    // When jump/up button is initially pressed, the duration it is held increments the extra_up_reg, which will allow yoshi
+    // to jump higher depending on the time the button is held. 
+    // Since jumping requires yoshi to be drawn differently depending on whether going up or down, how yoshi is drawn standing 
+    // or while walking is determined in the same FSM.
    
     // symbolic state representations
     localparam [2:0] standing   = 3'b000, // idle, no buttons pressed
@@ -332,17 +332,17 @@ module yoshi_sprite
        
             walking:
                 begin
-				if(!grounded)
-					begin
+		if(!grounded)
+		    begin
                     state_next_y = jump_down;           // go to jump down state
                     start_next_y = TIME_MAX_Y;          // load max time in start time reg
                     jump_t_next  = TIME_MAX_Y;          // load max time in jumpt time reg
                     end
 				
-				if((!btnL && !btnR) || (btnL && btnR))  // if no L/R buttons pressed, or both pressed
+		if((!btnL && !btnR) || (btnL && btnR))  // if no L/R buttons pressed, or both pressed
                     state_next_y = standing;                 // no more walking, go to standing state
 				
-				if(btnU_edge)                           // if up button pressed
+		if(btnU_edge)                           // if up button pressed
                     begin
                     state_next_y = jump_up;             // go to jump up state
                     start_next_y = TIME_START_Y;        // load start time in start time reg
@@ -378,16 +378,16 @@ module yoshi_sprite
                     if(btnU && start_reg_y > BEGIN_COUNT_EXTRA)   // if btnU still pressed, after certain time
                         extra_up_next = extra_up_reg + 1;         // increment extra up count
                     
-					if( s_y_next > MIN_Y)                         // if yoshi can go up
-						s_y_next = s_y_reg - 1;                   // move yoshi sprite up by one pixel
-                    else 										  // else if yoshi will hit ceiling
-						begin
-						state_next_y = jump_down;                 // go to jump down state
-						start_next_y = TIME_MAX_Y;                // load max time in start time reg
+		    if( s_y_next > MIN_Y)                 	// if yoshi can go up
+			s_y_next = s_y_reg - 1;                   // move yoshi sprite up by one pixel
+                else 				                  // else if yoshi will hit ceiling
+			begin
+			state_next_y = jump_down;                 // go to jump down state
+			start_next_y = TIME_MAX_Y;                // load max time in start time reg
                         jump_t_next  = TIME_MAX_Y;                // load max time in jump time reg
-						end
+			end
 						
-					if(start_reg_y <= TIME_MAX_Y)                 // if start time reg < maximum
+		    if(start_reg_y <= TIME_MAX_Y)                 // if start time reg < maximum
                         begin
                         start_next_y = start_reg_y + TIME_STEP_Y; // increment start time reg
                         jump_t_next = start_reg_y + TIME_STEP_Y;  // set jump time reg to new start value
@@ -422,12 +422,12 @@ module yoshi_sprite
                     begin
                     extra_up_next = extra_up_reg - 1;       // decrement extra jump up count reg
                     
-					if( s_y_next > MIN_Y)                   // if yoshi can go up
-						s_y_next = s_y_reg - 1;             // move yoshi sprite up by one pixel
+		    if( s_y_next > MIN_Y)                   // if yoshi can go up
+			s_y_next = s_y_reg - 1;             // move yoshi sprite up by one pixel
                     else 									// else if yoshi will hit ceiling
-						state_next_y = jump_down;           // go to jump down state
+			state_next_y = jump_down;           // go to jump down state
 	
-					start_next_y = TIME_MAX_Y;              // reset start time reg to max time
+		    start_next_y = TIME_MAX_Y;              // reset start time reg to max time
                     jump_t_next = TIME_MAX_Y;               // reset jump time reg to max time
                     end
                 end
@@ -482,7 +482,7 @@ module yoshi_sprite
     assign row = head_on  ? (y - s_y_reg):
                  torso_on ? (dy + y - s_y_reg): 0;
 				 
-	// either a normal yoshi or ghost yoshi is drawn depending on the game state routed into this module
+    // either a normal yoshi or ghost yoshi is drawn depending on the game state routed into this module
    
     // vector for ROM color_data output
     wire [11:0] color_data_yoshi, color_data_yoshi_ghost;
@@ -490,8 +490,8 @@ module yoshi_sprite
     // instantiate yoshi ROM circuit
     yoshi_rom yoshi_rom_unit (.clk(clk), .row(row), .col(col), .color_data(color_data_yoshi));
 	
-	// instantiate yoshi ghost ROM circuit
-	yoshi_ghost_rom yoshi_ghost_rom_unit (.clk(clk), .row(row), .col(col), .color_data(color_data_yoshi_ghost));
+    // instantiate yoshi ghost ROM circuit
+    yoshi_ghost_rom yoshi_ghost_rom_unit (.clk(clk), .row(row), .col(col), .color_data(color_data_yoshi_ghost));
 	
     // vector to signal when vga_sync pixel is within head or torso tile (see diagram)
     wire head_on, torso_on;
@@ -503,23 +503,23 @@ module yoshi_sprite
    
     // assign module output signals
     assign y_x = s_x_reg;
-	assign y_y = s_y_reg;
-	assign jumping_up = (state_reg_y == jump_up) ? 1 : 0;
-	assign direction = dir_reg;
+    assign y_y = s_y_reg;
+    assign jumping_up = (state_reg_y == jump_up) ? 1 : 0;
+    assign direction = dir_reg;
 	
-	// if collision occurs, turn yoshi into a ghost for 2 seconds (200000000 clock cycles);
-	reg [27:0] collision_time_reg;
-	wire [27:0] collision_time_next;
+    // if collision occurs, turn yoshi into a ghost for 2 seconds (200000000 clock cycles);
+    reg [27:0] collision_time_reg;
+    wire [27:0] collision_time_next;
 	
-	// infer ghost_yoshi_time register
-	always @(posedge clk, posedge reset)
-		if(reset)
-			collision_time_reg <= 0;
-		else 
-			collision_time_reg <= collision_time_next;
+    // infer ghost_yoshi_time register
+    always @(posedge clk, posedge reset)
+	if(reset)
+		collision_time_reg <= 0;
+	else 
+		collision_time_reg <= collision_time_next;
 	
-	// when collision occurs, set reg to 2 seconds of clk cycles, else decrement until 0
-	assign collision_time_next = collision ? 200000000 : 
+    // when collision occurs, set reg to 2 seconds of clk cycles, else decrement until 0
+    assign collision_time_next = collision ? 200000000 : 
 								 collision_time_reg > 0 ? collision_time_reg - 1 : 0;
        
     // rgb output
