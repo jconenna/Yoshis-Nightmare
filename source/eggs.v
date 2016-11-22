a6b8d1fbbb98da0da62519dc511d8d07e1fc7112
Joey Conenna <jconenna@knights.ucf.edu>
2016-11-22 13:56:42 -0500

    Update eggs.v

--- source/eggs.v
+++ source/eggs.v
@@ -7,7 +7,7 @@ module eggs
         output eggs_on,                    // on signal: vga pixel within sprite location
         output wire [11:0] rgb_out,        // output rgb signal for current pixel
         output wire [13:0] score,          // output score register value
-		output wire new_score              // output signal that is asserted when a new score is calculated
+	output wire new_score              // output signal that is asserted when a new score is calculated
     );
    
     // symbolic states for left and right motion of yoshi
@@ -16,9 +16,9 @@ module eggs
    
     // register to oscillate through platforms to spawn egg onto (see platform diagram)
     reg [2:0] platform_select_reg;
-	wire [2:0] platform_select_next;
+    wire [2:0] platform_select_next;
 	
-   // infer platform select register
+    // infer platform select register
     always @(posedge clk, posedge reset)
         if(reset)
             platform_select_reg <= 5;
@@ -47,8 +47,8 @@ module eggs
             end
            
     // platform location registers:
-	// when a new egg spawns, the platform_select_reg determines which platform, while the corresponding
-	// platform location registers determine where on the platform the new egg will spawn.
+    // when a new egg spawns, the platform_select_reg determines which platform, while the corresponding
+    // platform location registers determine where on the platform the new egg will spawn.
     reg[7:0] A_reg; // [ 16, 144]
     reg[9:0] B_reg; // [480, 608]
     reg[9:0] C_reg; // [ 80, 545]
@@ -90,56 +90,56 @@ module eggs
         else
             state_reg <= state_next;
 			
-	// new_score register, signals when a new score is calculated
-	reg new_score_reg, new_score_next; 
+    // new_score register, signals when a new score is calculated
+    reg new_score_reg, new_score_next; 
 	
-	// infer new_score register
-	always @(posedge clk, posedge reset)
-        if(reset)
-            new_score_reg <= 0;
-        else
+    // infer new_score register
+    always @(posedge clk, posedge reset)
+    if(reset)
+	    new_score_reg <= 0;
+    else
             new_score_reg <= new_score_next;
 	
-	// assign new_score output
+    // assign new_score output
     assign new_score = new_score_reg;
    
     // symbolic state representations for egg FSM
     localparam  waiting    = 1'b0, // waiting for yoshi to get egg
                 respawn    = 1'b1; // yoshi acquired egg, respawn new egg
     
-	// egg FSM next-state logic
+    // egg FSM next-state logic
     always @*
         begin
-		// defaults
+	// defaults
         state_next = state_reg;
         egg_x_next = egg_x_reg;
         egg_y_next = egg_y_reg;
         egg_type_next = egg_type_reg;
-		new_score_next = 0;
+	new_score_next = 0;
        
         case(state_reg)
            
             waiting: // egg already spawned, waiting for yoshi to get it
                 begin
-				// if yoshi collides with egg facing LEFT
+		// if yoshi collides with egg facing LEFT
                 if(direction == LEFT && (egg_x_reg + 13 > y_x && egg_x_reg < y_x + 13 && egg_y_reg + 13 > y_y && egg_y_reg < y_y + 13) ||
                   (egg_x_reg + 13 > y_x + 9 && egg_x_reg < y_x + 16 && egg_y_reg + 13 > y_y + 13 && egg_y_reg < y_y + 18))
                     begin
-					new_score_next = 1;    // signal new score ready
-					state_next = respawn; // go to respawn state
-					end
+		    new_score_next = 1;    // signal new score ready
+		    state_next = respawn; // go to respawn state
+		    end
                 
-				// else if yoshi collides with egg facing RIGHT
+		// else if yoshi collides with egg facing RIGHT
                 else if(direction == RIGHT && (egg_x_reg + 13 > y_x + 9 && egg_x_reg < y_x + 16 && egg_y_reg + 13 > y_y && egg_y_reg < y_y + 13) ||
                        (egg_x_reg + 13 > y_x && egg_x_reg < y_x + 13 && egg_y_reg + 13 > y_y + 13 && egg_y_reg < y_y + 18))
                     begin
-					new_score_next = 1;    // signal new score ready
-					state_next = respawn; // go to respawn state
-					end
+		    new_score_next = 1;    // signal new score ready
+		    state_next = respawn; // go to respawn state
+		    end
                 end
 
             respawn: // respawn new egg at current platform_select_reg platform,
-         			 // and at this platform's location register value
+         	     // and at this platform's location register value
                 begin
                 if(platform_select_reg == 0) // 'A'
                     begin
@@ -189,36 +189,36 @@ module eggs
         endcase
         end
    
-	// assign egg_type_offset depending on cycling egg_type_reg value
-	// Green, Red, Blue, Yellow, Special in order of decreasing probability
+    // assign egg_type_offset depending on cycling egg_type_reg value
+    // Green, Red, Blue, Yellow, Special in order of decreasing probability
     wire [6:0] egg_type_offset;
     assign egg_type_offset = (egg_type_reg <= 29) ? 0 :
                              (egg_type_reg > 29  && egg_type_reg <= 44) ? 16 :
                              (egg_type_reg > 44  && egg_type_reg <= 54) ? 32 :
                              (egg_type_reg > 54  && egg_type_reg <= 62) ? 48 : 64;
     
-	// score reg and next-state logic
-	reg [13:0] score_reg;
-	wire [13:0] score_next;
+    // score reg and next-state logic
+    reg [13:0] score_reg;
+    wire [13:0] score_next;
 	
-	// infer score register
-	always @(posedge clk, posedge reset)
-        if(reset)
-            score_reg <= 0;
-        else
+    // infer score register
+    always @(posedge clk, posedge reset)
+    if(reset)
+	    score_reg <= 0;
+    else
             score_reg <= score_next;
 	
-	// score updates on new_score_reg tick depending on egg_type_offset (egg color)
-	assign score_next = (reset || new_score_next && score_reg == 9999) ? 0 : 
-						new_score_next && egg_type_offset ==  0 ? score_reg +   20: // green   egg: p = 30/64, score =  10
-						new_score_next && egg_type_offset == 16 ? score_reg +   40: // red     egg: p = 15/64, score =  20
-						new_score_next && egg_type_offset == 32 ? score_reg +   60: // blue    egg: p = 10/64, score =  50
-						new_score_next && egg_type_offset == 48 ? score_reg +  200: // yellow  egg: p = 08/64, score =  100
-						new_score_next && egg_type_offset == 64 ? score_reg +  500: // special egg: p = 01/64, score =  500
-						score_reg;
+    // score updates on new_score_reg tick depending on egg_type_offset (egg color)
+    assign score_next = (reset || new_score_next && score_reg == 9999) ? 0 : 
+			new_score_next && egg_type_offset ==  0 ? score_reg +   20: // green   egg: p = 30/64, score =  10
+			new_score_next && egg_type_offset == 16 ? score_reg +   40: // red     egg: p = 15/64, score =  20
+			new_score_next && egg_type_offset == 32 ? score_reg +   60: // blue    egg: p = 10/64, score =  50
+			new_score_next && egg_type_offset == 48 ? score_reg +  200: // yellow  egg: p = 08/64, score =  100
+			new_score_next && egg_type_offset == 64 ? score_reg +  500: // special egg: p = 01/64, score =  500
+			score_reg;
 	
-	// assign score to output
-	assign score = score_reg;
+    // assign score to output
+    assign score = score_reg;
    
     // sprite coordinate addreses, from upper left corner
     // used to index ROM data
